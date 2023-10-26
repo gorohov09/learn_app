8d049874078a1e3ff2652c9bdcf00fa0529b324c
AndrewGorohov <andrej.gorokhoff2017@yandex.ru>
2023-10-26 18:21:47 +0300

    exercise_laba_4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ module ActiveSupport
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors, with: :threads)
 
+    include ApplicationHelper
+
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
